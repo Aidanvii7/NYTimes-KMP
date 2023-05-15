229a5c462b7430ed90fb63b4cd26170a07f89811
Aidan McWilliams <aidan.vii@googlemail.com>
2023-05-15 15:58:34 +0400

    Initialize ir/issue/backing-delegate

---
+++ app/ios-compose/src/iosMain/kotlin/io/github/xxfast/nytimes/ios/compose/Application.kt
@@ -0,0 +1,29 @@
+package io.github.xxfast.nytimes.ios.compose
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import io.github.xxfast.krouter.LocalComponentContext
+import io.github.xxfast.nytimes.data.appStorage
+import io.github.xxfast.nytimes.screens.home.HomeScreen
+import platform.Foundation.NSHomeDirectory
+import platform.UIKit.UIViewController
+
+val text: String @Composable get() = "Some text"
+
+fun Main(): UIViewController = ComposeUIViewController {
+  val lifecycle = LifecycleRegistry()
+  val rootComponentContext = DefaultComponentContext(lifecycle = lifecycle)
+  appStorage = NSHomeDirectory()
+
+  CompositionLocalProvider(LocalComponentContext provides rootComponentContext) {
+    MaterialTheme {
+      HomeScreen()
+      Text(text)
+    }
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NYTimes-KMP"
+
+include(":app")
+include(":app:android")
+include(":app:desktop")
+include(":app:ios-compose")
